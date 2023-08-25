c8319c977f7be504d1ef3f20cbe4ee8239d4c962
terrehdactyl <terrehdactyl@gmail.com>
2023-08-25 08:06:36 -0600

    testing pointer magic

--- deref.c
+++ deref.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-#define my_sizeof(X)  ((X*)0+1)
+
 void deref(char *t[])
 {
-	size_t size = my_sizeof(t);
-	printf("%d", size);
-  //  	for(int count = 1; t; count++,  t++)
-		// printf("variable: %s\ncount: %d\n\n", *t, count);
+   	for(int count = 1; *t; count++)
+		printf("variable: %s\ncount: %d\n\n", &**t++, count);
 }
 int main(void) 
 {

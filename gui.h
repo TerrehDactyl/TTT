11666e69ceaa79370cf4c194724bbd6265d90035
terrehdactyl <terrehdactyl@gmail.com>
2023-09-08 13:00:58 -0600

    removed scrolled window

--- gui.h
+++ gui.h
@@ -11,7 +11,6 @@ void pack_boxes(int i)
         gtk_box_pack_start(GTK_BOX(hbox), gwidget.label_grid, FALSE, FALSE, 0); 
         gtk_box_pack_start(GTK_BOX(hbox), gwidget.entry_grid, FALSE, FALSE, 0); 
         gtk_box_pack_start(GTK_BOX(vbox), gwidget.button_box, FALSE, FALSE, 0); 
-        gtk_box_pack_start(GTK_BOX(hbox), gwidget.scrolled_window, FALSE, FALSE, 0); 
         break;
 
         case 1://website scanning
@@ -59,16 +58,6 @@ void network_scanning() //i need to reorder this function, it's still a wreck
     gwidget.nested_frame = create_frame_with_pagehead(gwidget.nested_notebook, "Network Scanning");
     gwidget.child = gtk_notebook_get_nth_page (GTK_NOTEBOOK(gwidget.notebook), 0);                       
     gtk_notebook_set_tab_label(GTK_NOTEBOOK(gwidget.notebook), gwidget.child, gwidget.page_label);
-    GtkTextBuffer *buffer = gtk_text_buffer_new (NULL);
-    gwidget.scrolled_window = gtk_scrolled_window_new(NULL, NULL);
-    const gchar *text = {"Prefilter multiple pictures Preprocess\n"}; //not sure if scrolled window needed for this
-    gtk_text_buffer_set_text (buffer, text, -1);
-    gwidget.pscan_output = gtk_text_view_new_with_buffer (buffer);
-    gtk_text_view_set_wrap_mode (GTK_TEXT_VIEW (gwidget.pscan_output), GTK_WRAP_WORD); 
-    gtk_text_view_set_editable(GTK_TEXT_VIEW(gwidget.pscan_output), FALSE);
-    //gtk_widget_set_sensitive(GTK_WIDGET(gwidget.pscan_output),FALSE);
-    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (gwidget.scrolled_window), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC); 
-    gtk_container_add (GTK_CONTAINER (gwidget.scrolled_window), gwidget.pscan_output);
     pack_boxes(0);
 }
 
